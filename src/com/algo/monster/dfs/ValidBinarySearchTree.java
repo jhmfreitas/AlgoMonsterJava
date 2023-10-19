54c359e332d063e5ee579c9711d1f6be809168fb
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2023-10-20 00:36:34 +0100

    Adds missing explanation

--- src/com/algo/monster/dfs/ValidBinarySearchTree.java
+++ src/com/algo/monster/dfs/ValidBinarySearchTree.java
@@ -6,6 +6,16 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
 
+/**
+ * A binary search tree is a binary tree with the property that for any node, the value of this node is greater than any node in its left subtree and less than any node's value in its right subtree. In other words, an inorder traversal of a binary search tree yields a list of values that is monotonically increasing (strictly increasing).
+ *
+ * Given a binary tree, determine whether it is a binary search tree.
+ *
+ * Time Complexity: O(n) since we visit each node at most once and there are n nodes in the tree.
+ *
+ * Space Complexity: O(h) stack memory where h is the height of the tree, but in the worst case if we have a skewed tree, h is in O(n).
+ *
+ */
 class ValidBinarySearchTree {
     public static class Node<T> {
         public T val;
