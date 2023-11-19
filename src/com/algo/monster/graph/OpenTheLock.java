156178a9f4da44f83258dafc6633675016493eef
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2023-11-19 16:19:40 +0000

    Adds open the lock question

--- src/com/algo/monster/graph/OpenTheLock.java
+++ src/com/algo/monster/graph/OpenTheLock.java
@@ -2,6 +2,21 @@ package com.algo.monster.graph;
 
 import java.util.*;
 
+/**
+ * You are faced with a 4-wheel lock where each wheel contains the numbers '0' through '9'.
+ * Turning a wheel can either increase or decrease its number by one, wrapping around from '9' to '0' or vice versa.
+ * A single move involves rotating any one of the wheels by one slot.
+ *
+ * The lock starts with the combination '0000'. However, there are specific combinations termed as "deadends".
+ * If the lock lands on any of these deadend combinations, the wheels jam, making it impossible to proceed.
+ *
+ * Your task is to determine the least number of moves needed to reach a given target combination from the starting point without hitting any deadend.
+ * If reaching the target is impossible due to deadends, return -1.
+ *
+ * The time complexity is O(n), where n is the number of possible combinations (which is 10^4 == 10000 in this case).
+ *
+ * The space complexity is also O(n) as we store the steps to reach each combination which each takes O(1).
+ */
 class OpenTheLock {
     public static int numSteps(String targetCombo, List<String> trappedCombos) {
         int count = 0;
